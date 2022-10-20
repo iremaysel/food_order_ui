ed368235159360e072515b2023a2ed039263b2df
Roger Ordaz <rogerordaz98@gmail.com>
2022-10-19 20:01:51 -0500

    termionado de organizar a clena arch y el navBcubi

---
+++ lib/features/main_components/pages/bottom_navigator.dart
@@ -0,0 +1,83 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:food_order_ui/features/payments/presentation/pages/cart_page/cart_view.dart';
+import 'package:food_order_ui/features/products/presentation/pages/favorite_page/favorite_page_view.dart';
+import 'package:food_order_ui/features/products/presentation/pages/home_page/components/colors.dart';
+import 'package:food_order_ui/features/products/presentation/pages/home_page/components/size_config.dart';
+import 'package:food_order_ui/features/products/presentation/pages/home_page/home_page_view.dart';
+import 'package:food_order_ui/features/user_profile/presentation/pages/profile_page/profile_page_view.dart';
+import 'package:food_order_ui/features/search_products/presentation/pages/search_page/search_page_view.dart';
+
+import '../bloc/cubit/nav_bar_cubit_cubit.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final navigationKey = GlobalKey<CurvedNavigationBarState>();
+  // int index = 0;
+
+  final screen = [
+    const HomePageView(),
+    const SearchPageView(),
+    const CartView(),
+    const FavoritePageView(),
+    const ProfilePageView(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    // SystemChrome.setSystemUIOverlayStyle(
+    //     const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    SizeConfig().init(context);
+    final items = [
+      Icon(Icons.home, size: SizeConfig.blockSizeHorizontal! * 8.5),
+
+      /// 30.0
+      Icon(Icons.search, size: SizeConfig.blockSizeHorizontal! * 8.5),
+      Icon(Icons.shopping_cart, size: SizeConfig.blockSizeHorizontal! * 8.5),
+      Icon(Icons.favorite, size: SizeConfig.blockSizeHorizontal! * 8.5),
+      Icon(Icons.person, size: SizeConfig.blockSizeHorizontal! * 8.5),
+    ];
+
+    return BlocProvider(
+      create: (_) => NavBarCubitCubit(),
+      child: SafeArea(
+        child: Scaffold(
+          extendBody: true,
+          backgroundColor: Colors.white,
+          body: BlocBuilder<NavBarCubitCubit, NavBarCubitState>(
+            builder: (context, state) {
+              return screen[state.index];
+            },
+          ),
+          bottomNavigationBar: Theme(
+            data: Theme.of(context)
+                .copyWith(iconTheme: const IconThemeData(color: Colors.white)),
+            child: BlocBuilder<NavBarCubitCubit, NavBarCubitState>(
+              builder: (context, state) {
+                return CurvedNavigationBar(
+                  key: navigationKey,
+                  color: Colors.black45,
+                  backgroundColor: Colors.transparent,
+                  buttonBackgroundColor: buttonColor,
+                  height: SizeConfig.blockSizeVertical! * 8,
+                  animationCurve: Curves.easeInOut,
+                  animationDuration: const Duration(milliseconds: 400),
+                  index: state.index,
+                  items: items,
+                  onTap: (index) =>
+                      context.read<NavBarCubitCubit>().updateIndexNavBar(index),
+                );
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
